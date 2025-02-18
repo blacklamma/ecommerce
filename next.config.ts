f032831372d99740a3b43ef52ec74b2e0fb8c8a4
blacklamma <72339528+blacklamma@users.noreply.github.com>
2025-02-18 15:12:09 -0700

    fixing errors v3

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
-module.exports = { eslint: { ignoreDuringBuilds: true } };
+module.exports = {
+  eslint: { ignoreDuringBuilds: true },
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+};
 const nextConfig: NextConfig = {
   eslint: {
     dirs: ["pages", "utils"],
