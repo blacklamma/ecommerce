a547b472bdf3f42b53971dbaa52f12012e3d5ba6
blacklamma <72339528+blacklamma@users.noreply.github.com>
2025-02-18 14:12:48 -0700

    added add product page

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   eslint: {
     dirs: ["pages", "utils"],
   },
+  images: {
+    remotePatterns: [{ hostname: "images.unsplash.com" }],
+  },
 };
 
 export default nextConfig;
---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,59 @@
+import FormSubmitButton from "@/src/components/FormSubmitButton";
+import { prisma } from "@/src/lib/db/prisma";
+import { redirect } from 'next/navigation';
+
+export const metadata = {
+    title: "Add Product - tEst-Commerce"
+}
+
+async function addProduct(formData: FormData) {
+    "use server";
+
+    const name = formData.get('name')?.toString();
+    const description = formData.get('description')?.toString();
+    const imageUrl = formData.get('imageUrl')?.toString();
+    const price = Number(formData.get('price') || 0);
+
+    if (!name || !description || !imageUrl || !price) {
+        throw Error("Missing required fields");
+    }
+    await prisma.product.create({
+        data: { name, description, imageUrl, price }
+    })
+
+    redirect("/");
+}
+
+export default function AddProductPage() {
+    return (
+        <div>
+            <h1 className="text-lg mb-3">Add Product</h1>
+            <form action={addProduct}>
+                <input
+                    required
+                    name="name"
+                    placeholder="Name"
+                    className="input-bordered input mb-2 w-full"></input>
+                <textarea
+                    required
+                    name="description"
+                    placeholder="Description"
+                    className="textarea-bordered textarea mb-2 w-full"></textarea>
+                <input
+                    required
+                    name="imageUrl"
+                    placeholder="Image Url"
+                    type="url"
+                    className="input-bordered input mb-2 w-full"></input>
+                <input
+                    required
+                    name="price"
+                    placeholder="Price"
+                    type="number"
+                    className="input-bordered input mb-2 w-full"></input>
+                <FormSubmitButton className="btn-block">ADD</FormSubmitButton>
+            </form>
+
+        </div>
+    );
+}
