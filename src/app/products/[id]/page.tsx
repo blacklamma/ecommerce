6f49e20bbb231e086bb0614bbebe537a4316adb0
blacklamma <72339528+blacklamma@users.noreply.github.com>
2025-03-02 16:29:56 -0700

    same

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,45 +1,45 @@
-import { prisma } from "@/src/lib/db/prisma"
-import { notFound } from "next/navigation";
-import Image from "next/image";
 import PriceTag from "@/src/components/PriceTag";
+import { prisma } from "@/src/lib/db/prisma";
 import { Metadata } from "next";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 import { cache } from "react";
 import AddToCartButton from "./AddToCartButton";
 import { incrementProductQuantity } from "./actions";
 
 interface ProductPageProps {
     params: {
-        id: string,
-    }
+        id: string;
+    };
 }
 
 const getProduct = cache(async (id: string) => {
     const product = await prisma.product.findUnique({ where: { id } });
     if (!product) notFound();
     return product;
-})
+});
 
-export async function generateMetadata(
-    { params }: ProductPageProps
-): Promise<Metadata> {
-    const { id } = params;
+export async function generateMetadata({
+    params: { id },
+}: ProductPageProps): Promise<Metadata> {
     const product = await getProduct(id);
+
     return {
-        title: product.name + " - tEstCommerce",
+        title: product.name + " - Flowmazon",
         description: product.description,
         openGraph: {
             images: [{ url: product.imageUrl }],
         },
-    }
+    };
 }
 
-export default async function ProductPage(
-    { params }: ProductPageProps
-) {
-    const { id } = params;
+export default async function ProductPage({
+    params: { id },
+}: ProductPageProps) {
     const product = await getProduct(id);
+
     return (
-        <div className="flex flex-col lg:flex-row gap-4 lg:items-center">
+        <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
             <Image
                 src={product.imageUrl}
                 alt={product.name}
@@ -47,18 +47,17 @@ export default async function ProductPage(
                 height={500}
                 className="rounded-lg"
                 priority
-            ></Image>
+            />
+
             <div>
-                <h1 className="text-5xl font-bold">
-                    {product.name}
-                </h1>
-                <PriceTag price={product.price} className="mt-4"></PriceTag>
-                <p className="py-6">
-                    {product.description}
-                </p>
-                <AddToCartButton productId={product.id} incrementProductQuantity={incrementProductQuantity}></AddToCartButton>
+                <h1 className="text-5xl font-bold">{product.name}</h1>
+                <PriceTag price={product.price} className="mt-4" />
+                <p className="py-6">{product.description}</p>
+                <AddToCartButton
+                    productId={product.id}
+                    incrementProductQuantity={incrementProductQuantity}
+                />
             </div>
         </div>
-    )
-
+    );
 }
